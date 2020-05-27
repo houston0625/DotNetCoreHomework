ed88cfa309934df0be585de472a96a64f71cb725
YuShu <houston0625@hotmail.com>
2020-05-27 18:09:25 +0800

    自訂 SaveChanges() 的範例

---
+++ hw_api/Models/ContosoUniversityContext.Partial.cs .cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace hw_api.Models
+{
+    public partial class ContosoUniversityContext : DbContext
+    {
+
+        public override int SaveChanges()
+        {
+            var entities = this.ChangeTracker.Entries();
+
+            foreach (var entry in entities)
+            {
+                Console.WriteLine("Entity Name: {0}", entry.Entity.GetType().FullName);
+                Console.WriteLine("Status: {0}", entry.State);
+
+                if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.CurrentValues.SetValues(new { IsDeleted = true });
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.CurrentValues.SetValues(new { ModifiedOn = DateTime.Now });
+                }
+            }
+
+            return base.SaveChanges();
+
+        }
+    }
+}
